75fd670c4049e06816592177f5dbc27a1ff49fa8
AntoCic <anto.cic.127@gmail.com>
2023-07-06 22:23:45 +0200

    oo

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const select = document.getElementById("listDk");
+const phList = document.getElementById("phList");
 let lettere = ['a', 'b', "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "z"];
+let Cocktail = [];
 let CocktailsName = [];
 let Cocktailsista = [];
+let CocktaiName = [];
+let CocktaiImg = [];
 let x = 0;
+
 for (i = 0; i < lettere.length; i++) {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${lettere[i]}`)
     .then((response) => response.json())
     .then((data) => {
       const obj = data.drinks;
+      for (let key of Object.keys(obj)) {
+        // console.log(key);
+        Cocktail.push({id:obj[key].idDrink, nome:obj[key].strDrink, immage:obj[key].strDrinkThumb, bicchiere:obj[key].strGlass, IBA:obj[key].strIBA, misura1:obj[key].strMeasure1, ingrediente1:obj[key].strIngredient1, misura2:obj[key].strMeasure2, ingrediente2:obj[key].strIngredient2, misura3:obj[key].strMeasure3, ingrediente3:obj[key].strIngredient3, misura4:obj[key].strMeasure4, ingrediente4:obj[key].strIngredient4, misura5:obj[key].strMeasure5, ingrediente5:obj[key].strIngredient5, misura6:obj[key].strMeasure6, ingrediente6:obj[key].strIngredient6, misura7:obj[key].strMeasure7, ingrediente7:obj[key].strIngredient7, misura8:obj[key].strMeasure8, ingrediente8:obj[key].strIngredient8, misura9:obj[key].strMeasure9, ingrediente9:obj[key].strIngredient9, misura10:obj[key].strMeasure10, ingrediente10:obj[key].strIngredient10, metodo:obj[key].strInstructionsIT})
+      }
       let n = 0;
-      Object.keys(obj).forEach(() => {
-        CocktailsName.push(obj[n].strDrink);
-        n++;
-      });
+      
       x++;
       if (lettere.length == (x + 1)) {
+        // console.log(Cocktail); 
+        // Cocktail.forEach(() => {
+        //   CocktailsName.push(Cocktail.nome);
+        //   CocktaiImg.push(Cocktail.immage);
+        // });
+        for (let key of Object.keys(Cocktail)) {
+          CocktailsName.push(Cocktail[key].nome);   
+        }
         Cocktailsista = CocktailsName.sort();
         for (ii = 0; ii < Cocktailsista.length; ii++) {
           createOption(Cocktailsista[ii]);
+          createImgList(CocktaiImg[ii],CocktailsName[ii]);
         }
       };
     });
@@ -28,4 +43,13 @@ function createOption(value) {
   option.value = value;
   option.appendChild(document.createTextNode(value));
   select.appendChild(option);
+}
+function createImgList(urlImg, nameCk) {
+  const a = document.createElement("a");
+  a.classList.add('CkBox');
+  a.setAttribute("href", "www.thecocktaildb.com/api/json/v1/1/search.php?s="+nameCk);
+  const img = document.createElement("img");
+  img.setAttribute("src", urlImg);
+  a.appendChild(img);
+  phList.appendChild(a);
 }
